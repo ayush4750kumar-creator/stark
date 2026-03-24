a17d5c063158dce2240f9191e2b0f2dbcb58b16b
ayush4750kumar-creator <ayush4750kumar@gmail.com>
2026-03-25 00:22:14 +0530

    allow all origins for CORS

--- stockpulse-backend/server.js
+++ stockpulse-backend/server.js
@@ -14,17 +14,7 @@ app.use(helmet({ crossOriginEmbedderPolicy: false }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors({
-  origin: function(origin, callback) {
-    if (!origin) return callback(null, true);
-    if (
-      origin.endsWith('.vercel.app') ||
-      origin === 'http://localhost:3000' ||
-      origin === 'http://localhost:3001'
-    ) {
-      return callback(null, true);
-    }
-    callback(new Error('Not allowed by CORS'));
-  },
+  origin: true,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
