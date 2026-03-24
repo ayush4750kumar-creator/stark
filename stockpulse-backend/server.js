520b94b75b8739de012658cd46ed859164d831d8
ayush4750kumar-creator <ayush4750kumar@gmail.com>
2026-03-24 17:28:39 +0530

    fix CORS for vercel

--- stockpulse-backend/server.js
+++ stockpulse-backend/server.js
@@ -18,6 +18,7 @@ app.use(cors({
     process.env.FRONTEND_URL || "http://localhost:3000",
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://stark-three-rouge.vercel.app",
   ],
   credentials: true,
 }));
